e64980d28049fe4e48e4c01c9992d2fd59e6756c
Amelia Lim <limchornguan418@gmail.com>
2019-03-30 11:48:56 +0100

    add ngIf and ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[] = [];
+  showUserDetails: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.users = [
+      {
+        firstName: 'Maxence',
+        lastName: 'Bouret',
+        age: 27,
+        address: {
+          street: 'Carrot',
+          houseNumber: 42,
+          city: 'Lisbon'
+        }
+      },
+      {
+        firstName: 'Amelia',
+        lastName: 'Lim',
+        age: 24,
+        address: {
+          street: 'Tomato',
+          houseNumber: 18,
+          city: 'Berlin'
+        }
+      },
+      {
+        firstName: 'Min Sheng',
+        lastName: 'Ang'
+      }
+    ];
   }
-
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,10 +1,10 @@
 export interface User {
   firstName: string;
   lastName: string;
-  age: number;
-  address: {
-    street: string;
-    houseNumber: number;
-    city: string;
+  age?: number;
+  address?: {
+    street?: string;
+    houseNumber?: number;
+    city?: string;
   };
 }
